d4ceffc3abb845e82e5e11de6ed918738a0c3049
OnishiYoshikazu <49217420+OnishiYoshikazu@users.noreply.github.com>
2019-07-10 19:05:15 +0900

    Initial commit

---
+++ yubin/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace yubin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient();
+            var request = new RestRequest();
+            client.BaseUrl = new Uri("http://zipcloud.ibsnet.co.jp/api/search");
+
+            request.Method = Method.GET;
+            request.AddParameter("zipcode", textBox1.Text, ParameterType.GetOrPost);
+
+            var response = client.Execute(request);
+            string json = response.Content.ToString();
+            var ret = JsonConvert.DeserializeObject<yubincode>(json);
+
+            if (ret.message == null) {
+                label2.Text = ret.results[0].address1 + ret.results[0].address2 + ret.results[0].address3;
+            }
+            else
+            {
+                label2.Text = "エラー！";
+            }
+        }
+    }
+}
---
+++ yubin/yubincode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yubin
+{
+
+    public class yubincode
+    {
+        public object message { get; set; }
+        public Result[] results { get; set; }
+        public int status { get; set; }
+    }
+
+    public class Result
+    {
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string address3 { get; set; }
+        public string kana1 { get; set; }
+        public string kana2 { get; set; }
+        public string kana3 { get; set; }
+        public string prefcode { get; set; }
+        public string zipcode { get; set; }
+    }
+
+}
